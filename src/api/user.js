3a5544aa7c864f39bf28eadc0bee967d54bbc3ec
zhou <z17333262851@163.com>
2023-08-01 16:22:16 +0800

    完成基础配置

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,13 @@
-export default {
-  namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+// import request from '@/utils/request'
+
+export function login(data) {
+
+}
+
+export function getInfo(token) {
+
+}
+
+export function logout() {
+
 }
--- vue.config.js
+++ vue.config.js
@@ -35,6 +35,12 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
+    },
+    proxy: {
+      'api': {
+        target: 'http://ihrm-java.itheima.net/', // 跨域请求的地址
+        changeOrigin: true // 只有这个值为true的情况下 才表示开启跨域
+      }
     }
     // before: require('./mock/mock-server.js')
   },
